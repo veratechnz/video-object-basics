2eebd9d31bc45ba97b05497854b1a837ed5e9a87
Jared Neems <jared.neems@cityrdbclab5017.auckland.natcoll.ac.nz>
2016-06-10 15:58:15 +1200

    this

---
+++ js/custom.js
@@ -0,0 +1,60 @@
+// Custom Javascript Here
+//1. There is a problem with the code.
+//2. Place the spaghetti code in function and invoke.
+//3. Use a parameter and an argument for one of your functions.
+//4. Place all functions, variables and object - in an object. Invoke the methods 	
+
+// 1. Spag & Broken Code
+// var video = document.createElement('video');
+// video.src = palm.mp4';
+// video.autoPlay = true;
+// video.play();
+// document.body.appendChild(video);
+// var videoPath = 'palms.mp4';
+
+
+// 2. and 3. Placed in a function and invoked
+// var refObj = {
+// 	videoPath: 'palms.mp4'
+// }
+
+// function createVideo(whatVideo){
+// 	var video = document.createElement('video');
+// 	video.src = 'palms.mp4';
+// 	video.autoPlay = true;
+// 	video.play();
+// 	document.body.appendChild(video);	
+// }
+
+// createVideo(refObj.videoPath);
+
+// 4. Final. Placed in an object
+// var videoObject = {
+// 	videoPath: 'palms.mp4', 
+// 	create: function(whatPath){
+// 		var video = document.createElement('video');
+// 		video.src = whatPath;
+// 		video.autoPlay = true;
+// 		video.play();
+// 		document.body.appendChild(video);	
+// 	}
+// };
+
+// videoObject.create(videoObject.videoPath);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
